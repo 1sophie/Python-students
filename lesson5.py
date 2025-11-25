041093485eba8cd0d814d93724de4e62a0e3fea4
MefeToma <tomasepashvili@gmail.com>
2025-11-25 16:10:05 +0400

    update lesson5

---
+++ lesson5.py
@@ -0,0 +1,8 @@
+item = ["name", "code", "quantity"]
+headphone = ["sony", "2233", 5]
+phone = ["iphone", "3232", 3]
+a = input("which one do you want, headphone or phone: ")
+if a == "headphone":
+    print(headphone)
+elif a == "phone":
+    print(phone)
