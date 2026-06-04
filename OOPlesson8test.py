8100d32656f58c064199fe63ebb1b2b19c55e83b
MefeToma <tomasepashvili@gmail.com>
2026-06-04 12:57:21 +0400

    Update OOPlesson8test.py

--- OOPlesson8test.py
+++ OOPlesson8test.py
@@ -14,14 +14,14 @@ class Student(Person):
         self.uni_year = uni_year
     def info(self):
         print(f"hi im {self.fname} {self.lname}, I graduated from {self.school} in {self.year} and I've got accepted at {self.uni} in {self.uni_year}")
-p1 = Person("irakli", "migineishvili")
-p2 = Person("tornike", "kalandarishvili")
-p3 = Person("aleqsandre", "abramishvili")
-p4 = Person("luka", "toidze")
-p1.printname
-p2.printname
-p3.printname
-p4.printname
+p1 = Student("irakli", "migineishvili", "", "", "", "")
+p2 = Student("tornike", "kalandarishvili", "", "", "", "")
+p3 = Student("aleqsandre", "abramishvili", "", "", "", "")
+p4 = Student("luka", "toidze", "", "", "", "")
+p1.printname()
+p2.printname()
+p3.printname()
+p4.printname()
 data = {
     "Student1" : {
         "Name" : p1.fname,
@@ -48,14 +48,20 @@ while True:
         find_name = input("which student: ")
         for key,value in data.items():
             
-            if value["name"] == find_name:
+            if value["Name"] == find_name:
                 grad_school = input("write your school name: ")
                 grad_year = int(input("write the year when you graduated: "))
                 uni = input("which university did you get accepted in: ")
                 uni_year = int(input("when did you get accepted: "))
+                value["school"] = grad_school
+                value["graduation year"] = grad_year
+                value["university"] = uni
+                value["university year"] = uni_year
+                print(value)
 
     elif person_add == "no":
         print("have a nice day")
+        print(data)
         break
     else:
         print("invalid")
